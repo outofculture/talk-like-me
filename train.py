0a794281b552b0ad33dca9e6c548033d8cf853d0
Martin Chase <outofculture@gmail.com>
2018-04-03 11:19:36 -0700

    cleaning, playing

--- train.py
+++ train.py
@@ -6,10 +6,9 @@ from keras import Input, Model
 from keras.backend import categorical_crossentropy, sigmoid, relu
 from keras.layers import Dense
 from keras.optimizers import SGD
-from scipy.fftpack import fft, ifft
 from scipy.signal import stft, istft
 
-from audio import load_digits, play_all, play
+from audio import load_digits, play_all
 
 sample_rate = 8000
 
@@ -36,7 +35,7 @@ image_input = Input(shape=input_shape)
 # MaxPooling2D(pool_size=(1, 10))()  # outputs (1, 3, 100)
 
 # Compress with a fully-connected layer
-encoded = Dense(1, activation=relu)(image_input)
+encoded = Dense(32, activation=relu)(image_input)
 # TODO try not compressing the data at all
 
 ####### Decode ########
@@ -59,22 +58,8 @@ autoencoder.compile(
 
 
 def shape_data_for_processing(data):
-    # TODO normalize the data (subtract mean, divide by stdev)
+    # TODO normalize the data? (subtract mean, divide by stdev)
     return stft(data, sample_rate, nperseg=samples_per_window, noverlap=samples_per_step * 4)[2]
-    # chunked_data = np.empty((data.shape[0],) + input_shape)
-    # for i in range(data.shape[0]):
-    #     spoken_word = data[i]
-    #     chunks = chunked_data[i]
-    #     for j in range(steps):
-    #         step_start = j * samples_per_step
-    #         range_start = ceil(step_start - ((samples_per_window - samples_per_step) / 2))
-    #         range_end = range_start + samples_per_window
-    #         if range_start < 0 or range_end > sample_rate:
-    #             continue
-    #         chunks[j] = window * np.abs(fft(
-    #             spoken_word[range_start:range_end]))[:samples_per_window // 2]
-    # # return np.concatenate(chunked_data)
-    # return chunked_data, labels
 
 
 def shape_prediction_for_playing(prediction):
@@ -83,26 +68,6 @@ def shape_prediction_for_playing(prediction):
     :return: list of 8000-long sound samples
     """
     return istft(prediction, sample_rate, nperseg=samples_per_window, noverlap=samples_per_step * 4)[1]
-    # sounds = np.zeros((prediction.shape[0], sample_rate * 5))
-    # for i in range(prediction.shape[0]):
-    #     for j in range(prediction.shape[1]):
-    #         sounds[i, j * samples_per_window:(j + 1) * samples_per_window] = np.abs(
-    #             ifft(np.concatenate((
-    #                 prediction[i, j],
-    #                 np.flip(prediction[i, j], 0),
-    #             ))))
-    #         # first_fft_that_applies = j - 2
-    #         # last_fft_that_applies = j + 2
-    #         # if first_fft_that_applies < 0:
-    #         #     first_fft_that_applies = 0
-    #         # if last_fft_that_applies >= prediction.shape[1]:
-    #         #     last_fft_that_applies = prediction.shape[1] - 1
-    #         # sounds[i, j * samples_per_step:(j + 1) * samples_per_step] = np.average(
-    #         #     np.abs(
-    #         #         ifft(prediction[i, first_fft_that_applies:last_fft_that_applies])),
-    #         #     axis=0,
-    #         # )[samples_per_step * 2:samples_per_step * 3]
-    # return sounds
 
 
 if __name__ == '__main__':
